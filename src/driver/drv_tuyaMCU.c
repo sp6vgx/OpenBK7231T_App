9b434635fe14e0b1dd26fe6a7bf56a4dc96e46b2
SP6VGX <>
2026-01-28 06:43:37 +0100

    Definitions of necessary functions have been added...

--- src/driver/drv_tuyaMCU.c
+++ src/driver/drv_tuyaMCU.c
@@ -14,12 +14,20 @@ void TuyaMCU_OnEverySecond() {
 void TuyaMCU_Shutdown() {
 }
 
+/*************************************************************  
+* Functions called from external code 
+**************************************************************/
+
 
 bool TuyaMCU_IsChannelUsedByTuyaMCU(int channel) {
-
 	return false;
 }
 
 void TuyaMCU_OnChannelChanged(int channel, int iVal) {
+}
 
+bool TuyaMCU_IsLEDRunning() {
+}
+
+void TuyaMCU_OnRGBCWChange(const float *rgbcw, int bLightEnableAll, int iLightMode, float brightnessRange01, float temperatureRange01) {
 }
